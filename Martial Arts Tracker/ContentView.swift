df00b51314571b609d7bd97062cd527d2a6a59f3
Shane <shwoods35@gmail.com>
2022-12-23 00:36:51 -0500

    initial commit

---
+++ Martial Arts Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Martial Arts Tracker
+//
+//  Created by Shane Woods on 12/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Martial Arts Tracker/Martial_Arts_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Martial_Arts_TrackerApp.swift
+//  Martial Arts Tracker
+//
+//  Created by Shane Woods on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Martial_Arts_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
